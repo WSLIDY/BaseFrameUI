343e4b55397379a581820ee6727018d85eab5aff
WSLIDY <771483643@qq.com>
2018-11-19 14:07:46 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/ljt/baseframeui/MainActivity.java
@@ -0,0 +1,33 @@
+package cn.com.ljt.baseframeui;
+
+import android.view.View;
+
+import cn.com.ljt.baseuilibrary.activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void addListener() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    public void widgetClick(View v) {
+
+    }
+}
